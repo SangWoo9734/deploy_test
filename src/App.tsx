754f89e1ae2751b05edf653ba7e5b29419fa279e
Parksangwoo <psu8430@gmail.com>
2024-07-01 03:08:53 +0900

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          제발 배포해주세요
         </a>
       </header>
     </div>
